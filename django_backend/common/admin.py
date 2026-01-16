5bfdee3666e1f3bc92241a456551db121634d426
SvanteRomero <129661441+SvanteRomero@users.noreply.github.com>
2026-01-16 14:48:17 +0300

    ..

--- django_backend/common/admin.py
+++ django_backend/common/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Brand, Location
+from .models import Brand, Location, Model
 
 admin.site.register(Brand)
 admin.site.register(Location)
+@admin.register(Model)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'brand')
+    list_filter = ('brand',)
+    search_fields = ('name', 'brand__name')
